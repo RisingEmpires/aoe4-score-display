36a024c4a9a2b557bc24dff05766c886aab9a4b8
Coow <eirik_skarding@hotmail.no>
2023-10-19 15:06:18 +0200

    Add extraInfo to Score (Hidden by default)

--- src/graphics/Index.tsx
+++ src/graphics/Index.tsx
@@ -15,6 +15,9 @@ export function Index() {
 	const [betweenText, set_betweenText] = useReplicant<string>('betweenText', '')
 	const [flipScore, set_flipScore] = useReplicant<boolean>('flipScore', false);
 
+	const [extraInfo1, set_extraInfo1] = useReplicant<string>('extraInfo1', '')
+	const [extraInfo2, set_extraInfo2] = useReplicant<string>('extraInfo2', '')
+
 	const [theme, set_theme] = useReplicant<{ value: string; label: string; }>('theme', { value: '../../../assets/nodecg-themer/themes/default.css', label: 'default' }, { namespace: 'nodecg-themer' });
 
 	const [themeDiv, set_themeDiv] = useState(<></>)
@@ -42,6 +45,8 @@ export function Index() {
 
 				<h1 className='score-betweenText'>{betweenText}</h1> </div> : <> </>}
 			{showSpoilerOverlay ? <img src={SpoilerCover} className={'score-spoilerCover'} /> : <> </>}
+			<h1 className='score-extraInfo score-extraInfo1 hidden absolute'>{extraInfo1}</h1>
+			<h1 className='score-extraInfo score-extraInfo2 hidden absolute'>{extraInfo2}</h1>
 		</>
 	);
 }
